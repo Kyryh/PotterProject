476ea11a6261918f3d51ff964a1d546a90e0055e
Donov Kyrylo <donovkyrylo@istitutocorni.it>
2024-02-26 09:49:43 +0100

    Finished JSONPair class

--- src/potterproject/JSONPair.java
+++ src/potterproject/JSONPair.java
@@ -5,4 +5,24 @@ public class JSONPair {
     private String param;
 
     private Object value;
+
+    public JSONPair(String param, Object value) {
+        this.param = param;
+        this.value = value;
+    }
+
+    public String getParam() {
+        return param;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "\"%s\"=%s".formatted(param, value);
+    }
+    
+    
 }
